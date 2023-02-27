7d375eed52b602098c6ce5373033797ca4d43f62
RivaniaGomes <rivaniagomes@gmail.com>
2023-02-26 21:37:22 -0300

    Conhecendo heranças

--- br.com.bank/src/br/com/bank/App.java
+++ br.com.bank/src/br/com/bank/App.java
@@ -1,6 +1,7 @@
 package br.com.bank;
 
-import br.com.bank.model.ContaBancaria;
+import br.com.bank.model.ContaCorrente;
+import br.com.bank.model.ContaPoupanca;
 
 public class App {
 
@@ -8,7 +9,7 @@ public class App {
 		System.out.println("Criando nosso Banco Digital");
 		System.out.println();
 
-		ContaBancaria conta = new ContaBancaria("0001", "7542", 5, 100.0);
+		ContaCorrente conta = new ContaCorrente("0001", "7542", 5, 100.0);
 		
 		System.out.println("O saldo atual é de R$ " + conta.getSaldo());
 		System.out.println();
@@ -18,6 +19,15 @@ public class App {
 		System.out.println();
 		
 		conta.sacar(150.0);
+		System.out.println("O saldo atual é de R$ " + conta.getSaldo());
+		System.out.println();
+		
+		ContaPoupanca conta2 = new ContaPoupanca("0001", "7543", 6, 200.0);
+		
+		conta2.transferir(100.0, conta);
+		System.out.println("O saldo conta destino é de R$ " + conta2.getSaldo());
+		System.out.println();
+		
 		System.out.println("O saldo atual é de R$ " + conta.getSaldo());
 		System.out.println();
 	}
---
+++ br.com.bank/src/br/com/bank/model/ContaPoupanca.java
@@ -0,0 +1,10 @@
+package br.com.bank.model;
+
+public class ContaPoupanca extends ContaBancaria{
+
+	public ContaPoupanca(String agencia, String conta, Integer digito, Double saldoInicial) {
+		super(agencia, conta, digito, saldoInicial);
+		// TODO Auto-generated constructor stub
+	}
+
+}
